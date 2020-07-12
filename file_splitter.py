11b17860f6cb27b49227e9023760a355e77e80cb
Meng Du <mengdu@umich.edu>
2020-07-11 17:36:45 -0700

    updates

--- file_splitter.py
+++ file_splitter.py
@@ -3,7 +3,7 @@ import os
 folder = 'google_api_transcription'
 if not os.path.isdir(folder):
     os.mkdir(folder)
-with open('google_api_transcription4.txt', 'r') as infile:
+with open('transcripts2.log', 'r') as infile:
     filename = ''
     counter = -1
     for line in infile:
@@ -12,6 +12,6 @@ with open('google_api_transcription4.txt', 'r') as infile:
             assert int(index) > counter
             counter = int(index)
         else:
-            with open(folder + '/' + filename.split('.')[0] + '.txt', 'w') as outfile:
+            with open(folder + '/' + filename.rsplit('.', 1)[0] + '.txt', 'w') as outfile:
                 outfile.writelines([filename, line])
             filename = ''
--- transcribe.py
+++ transcribe.py
@@ -10,16 +10,26 @@
 #  to Google Cloud Storage. On line 95 set local=False, long=True. Also make sure
 #  sample rate (line 50) is correct before transcribing.
 
-from google.cloud import speech_v1
+from google.cloud import speech_v1 as speech
 from google.api_core.exceptions import InvalidArgument
 import wave
 import audioop
+from pydub import AudioSegment
 import os
 import logging
 from tqdm import tqdm
 
 
-def convert2mono(local_file_path, out_file_path=None):
+def convert2mono(local_file_path, out_file_path=None, ext='wav'):
+    if ext != 'wav':
+        audio = AudioSegment.from_file(local_file_path, ext)
+        if out_file_path is None:
+            local_file_path = local_file_path.replace(ext, 'wav')
+            out_file_path = local_file_path
+        else:
+            local_file_path = out_file_path
+        audio.export(out_file_path, format='wav')
+
     # convert audio to one channel (mono)
     with wave.open(local_file_path, 'rb') as f:
         rate = f.getframerate()
@@ -34,20 +44,18 @@ def convert2mono(local_file_path, out_file_path=None):
     return rate, audio
 
 
-def recognize(file_path, logger, local=True, long=False):
+def recognize(file_path, logger, local=True, long=False, ext='wav'):
     """
     Transcribe a short audio file using synchronous speech recognition
-    Args:
-      local_file_path Path to local audio file, e.g. /path/audio.wav
     """
 
-    client = speech_v1.SpeechClient()
+    client = speech.SpeechClient()
 
     if local:
         # convert audio to one channel (mono)
-        rate, audio = convert2mono(file_path)
+        rate, audio = convert2mono(file_path, ext=ext)
     else:
-        rate = 48000
+        rate = 44100
 
     config = {
         "language_code": "en-US",  # The language of the supplied audio
@@ -71,12 +79,14 @@ def recognize(file_path, logger, local=True, long=False):
 
 def main():
     # transcribing local files
-    # audiopath = 'audio/'
-    # filenames = sorted(os.listdir('audio'))
+    # audiopath = 't20_recordings/'
+    # extension = 'm4a'
+    # filenames = sorted([f for f in os.listdir(audiopath) if f.endswith(extension)])
 
     # transcribing files on Google Cloud Storage
     audiopath = 'gs://dorm_study_audio/audio_mono/'
     filenames = sorted(os.listdir('audio_mono'))
+    extension = 'wav'
 
     # set up logs
     transcripts_logger = logging.getLogger('transcripts')
@@ -88,30 +98,41 @@ def main():
 
     # transcribe
     for i, f in enumerate(tqdm(filenames)):
-        if f.endswith('.wav'):
+        if f.endswith(extension):
             api_logger.info(f)
             transcripts_logger.info(str(i) + ',' + f)
             try:
-                transcript = recognize(audiopath + f, api_logger, local=False, long=True)
+                transcript = recognize(audiopath + f, api_logger,
+                                       local=False, long=True, ext=extension)
                 transcripts_logger.info(transcript)
             except Exception as e:
                 transcripts_logger.error(str(e))
 
 
-def batch_conversion():
-    audiopath = 'audio/'
+def batch_conversion(filenames=None, ext='wav'):
+    """
+    filenames: (string) txt file containing filenames (one per line)
+    """
+    audiopath = 't20_recordings/'
     outpath = 'audio_mono/'
-    with open('long_missing.txt', 'r') as f:
-        filenames = f.readlines()
-    filenames = [f.strip('\n') for f in filenames]
+    if not os.path.isdir(outpath):
+        os.mkdir(outpath)
+    if filenames:
+        with open(filenames, 'r') as f:
+            filenames = f.readlines()
+        filenames = [f.strip('\n') for f in filenames]
+    else:
+        filenames = os.listdir(audiopath)
     for f in tqdm(filenames):
         try:
-            rate, audio = convert2mono(audiopath + f, outpath + f)
-            # print(rate)
+            rate, audio = convert2mono(audiopath + f,
+                                       outpath + f.replace(ext, 'wav'),
+                                       ext)
+            print(rate)
         except Exception as e:
             print(f, e)
 
 
 if __name__ == "__main__":
-    # batch_conversion()
+    # batch_conversion('long.txt', ext='m4a')
     main()
